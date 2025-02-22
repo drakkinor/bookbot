31a3f6d2b1fdaa5d92513242a02cff388819c140
drakkinor <joseph.a.hughes@gmail.com>
2025-02-22 16:47:33 -0600

    added get_char_counts

--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(text):
     word_list = text.split()
     return len(word_list)
+
+def get_char_counts(text):
+    char_list = list(text.lower())
+    #print(char_list)
+    char_counts = {}
+    for item in char_list:
+        char_counts[item] = char_counts.get(item, 0) + 1
+    char_counts = dict(sorted(char_counts.items()))
+    #print(char_counts)
+    return char_counts
+  
