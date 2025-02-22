a6dd9549cba6205bb5e2c057d97609e3f392a66c
drakkinor <joseph.a.hughes@gmail.com>
2025-02-22 16:05:54 -0600

    renamed word count to get_num_words plus refactoring

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
     # Opens the passed filepath and returns the contents as string
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
-def count_words(text):
-    word_list = []
-    #print(word_list)
-    word_list = text.split()
-    #print(word_list)
-    return len(word_list)
-    #pass
-
-
 def main():
-    count = count_words(get_book_text("./books/frankenstein.txt"))
+    count = get_num_words(get_book_text("./books/frankenstein.txt"))
     print(f"{count} words found in the document")
 
 main()
