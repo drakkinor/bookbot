abf206f33872e03ca26a2fbbbeb81cd0a1808981
drakkinor <joseph.a.hughes@gmail.com>
2025-02-23 16:15:00 -0600

    added get_sorted_char_list to use in report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_char_counts
+from stats import get_num_words, get_char_counts, get_sorted_char_list
 
 def get_book_text(filepath):
     # Opens the passed filepath and returns the contents as string
@@ -7,9 +7,9 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    words = get_num_words(get_book_text("./books/frankenstein.txt"))
-    print(f"{words} words found in the document")
+    #words = get_num_words(get_book_text("./books/frankenstein.txt"))
+    #print(f"{words} words found in the document")
     char_count = get_char_counts(get_book_text("./books/frankenstein.txt"))
-    print(char_count)
+    get_sorted_char_list(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -3,12 +3,34 @@ def get_num_words(text):
     return len(word_list)
 
 def get_char_counts(text):
+    # returns dictionary of all detected characters and their counts in order characters appear
     char_list = list(text.lower())
     #print(char_list)
     char_counts = {}
     for item in char_list:
         char_counts[item] = char_counts.get(item, 0) + 1
-    char_counts = dict(sorted(char_counts.items()))
+    #char_counts = dict(sorted(char_counts.items()))
     #print(char_counts)
     return char_counts
-  
+
+def get_sorted_char_list(dictionary):
+    #returns a list of dictionaries of all alpha characters in order of highest count
+   
+    #sorting the input dictionary by value and creating a new dictionary with the results
+    sorted_dict = dict(sorted(dictionary.items(), reverse=True, key=lambda x:x[1]))
+    #print(sorted_dict)
+
+    #instantiate return list
+    sorted_list = []
+
+    #iterate sorted dictionary to create and append new single-entry dictionaries for each alpha kv pair
+    for item in sorted_dict:
+        if item.isalpha():
+            mydict = {}
+            mydict[item] = sorted_dict[item]
+            sorted_list.append(mydict)
+    
+    #print(sorted_list)
+    return sorted_list
+
+    
